0928de8cb4ad12b3d1448ac524e0d1c13442966c
Igor <igor.nikoloov@gmail.com>
2016-08-05 03:46:35 +0200

    Admin
    Dodavajne na Admin

--- app/src/main/java/com/example/igor/restaurantapp/RestaurantHome.java
+++ app/src/main/java/com/example/igor/restaurantapp/RestaurantHome.java
@@ -1,50 +1,65 @@
 package com.example.igor.restaurantapp;
 
-        import android.app.Activity;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.Menu;
-        import android.view.MenuItem;
-        import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-        import com.example.igor.restaurantapp.Database.DBHelper;
-        import com.example.igor.restaurantapp.Service.MyService;
+import com.example.igor.restaurantapp.Database.DBHelper;
+import com.example.igor.restaurantapp.Service.MyService;
 
 
-        import java.util.ArrayList;
-        import java.util.List;
-        import android.widget.AdapterView;
-        import android.widget.ArrayAdapter;
-        import android.widget.AdapterView.OnItemClickListener;
-        import android.widget.Button;
-        import android.widget.ListView;
-        import android.view.KeyEvent;
-        import android.view.Menu;
-        import android.view.MenuItem;
-        import android.view.View;
+import java.util.ArrayList;
+import java.util.List;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class RestaurantMain extends Activity {
+public class RestaurantHome extends Activity {
     String msg = "Android : ";
-
+    public final static String EXTRA_MESSAGE = "MESSAGE";
+    private ListView obj;
+    DBHelper mydb;
 
     /** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurant_main);
+        setContentView(R.layout.admin_main);
 
-        Button btn=(Button)findViewById(R.id.button);
+        mydb = new DBHelper(this);
 
-        btn.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                Intent myIntent = new Intent(getApplicationContext() , RestaurantHome.class);
-                startActivity(myIntent);
+
+        ArrayList array_list = mydb.getAllCotacts();
+        final ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1, array_list);
+
+        obj = (ListView)findViewById(R.id.listView1);
+        obj.setAdapter(arrayAdapter);
+        obj.setOnItemClickListener(new OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+                // TODO Auto-generated method stub
+                int id_To_Search = arg2 + 1;
+
+                Bundle dataBundle = new Bundle();
+
+                dataBundle.putInt("id", id_To_Search);
+
+                Intent intent = new Intent(getApplicationContext(),DisplayAdimin.class);
+
+                intent.putExtras(dataBundle);
+                startActivity(intent);
+                Log.d(msg, "The onCreate() event");
             }
         });
-
     }
     /** Called when the activity is about to become visible. */
     @Override
@@ -94,8 +109,7 @@ public class RestaurantMain extends Activity {
 
         switch(item.getItemId())
         {
-            case R.id.item1:
-                Bundle dataBundle = new Bundle();
+            case R.id.item1:Bundle dataBundle = new Bundle();
                 dataBundle.putInt("id", 0);
 
                 Intent intent = new Intent(getApplicationContext(),DisplayAdimin.class);
@@ -104,12 +118,9 @@ public class RestaurantMain extends Activity {
                 startActivity(intent);
                 return true;
 
-         /*   case R.id.item2:
                 Intent intent1 = new Intent(getApplicationContext(),RestaurantHome.class);
                 startActivity(intent1);
                 return true;
-
-         */
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -131,8 +142,4 @@ public class RestaurantMain extends Activity {
         }
         return super.onKeyDown(keycode, event);
     }
-
-
 }
-
-
